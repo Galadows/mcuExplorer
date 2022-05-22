7e3dd789a4a5e6f15811ce52e5a1f327ed32b4b7
Havane <havane.palabaud@epitech.eu>
2022-05-22 14:17:11 +0200

    Banner turned into a subComponent

--- components/Welcome.vue
+++ components/Welcome.vue
@@ -2,59 +2,10 @@
   <div
     class="flex border-white border-4 flex-col justify-center items-center h-screen w-screen bg-marvel-red overflow-hidden"
   >
-    <div
-      class="flex flex-1 justify-center items-center animate-infinite-scroll"
-      :class="{ pause: upBannerHover }"
-    >
-      <a
-        v-for="movie in movies"
-        :key="movie.title + 1"
-        @mouseenter="
-          () => {
-            upBannerHover = true
-            isHovered = 'upBanner-' + movie.id
-          }
-        "
-        @mouseleave="
-          () => {
-            upBannerHover = false
-            isHovered = ''
-          }
-        "
-        @click="selectOnTimelineEmiter(movie.id)"
-        class="w-[172.77px] h-[256px] m-10 animate-fade-in"
-      >
-        <h1
-          class="absolute w-[172.77px] h-[256px] text flex justify-center items-center text-center z-10 text-white font-bold pointer-events-none px-2"
-          :class="{
-            'animate-fade-in-fast': isHovered == 'upBanner-' + movie.id,
-            hidden: isHovered != 'upBanner-' + movie.id,
-          }"
-        >
-          {{ movie.title }}
-        </h1>
-        <img
-          :src="movie.cover_url"
-          :alt="movie.title + ' cover'"
-          class="h-full md:w-full rounded-sm animate-unblur hover:animate-blur cursor-pointer"
-        />
-      </a>
-      <a
-        v-for="movie in movies"
-        :key="movie.title + 2"
-        @click="selectOnTimelineEmiter(movie.id)"
-        class="w-[172.77px] h-[256px] m-10 animate-fade-in"
-      >
-        <img
-          @mouseenter="upBannerHover = true"
-          @mouseleave="upBannerHover = false"
-          :src="movie.cover_url"
-          :alt="movie.title + ' cover'"
-          class="h-full md:w-full rounded-sm animate-unblur hover:animate-blur cursor-pointer"
-        />
-      </a>
-    </div>
-
+    <scrolling-banner
+      :images="movies"
+      @imageClicked="(id) => selectOnTimelineEmiter(id)"
+    />
     <div class="flex flex-2 flex-row">
       <a
         @mouseenter="buttonHover = true"
@@ -65,47 +16,20 @@
         {{ buttonHover ? 'Explore the MCU' : 'Welcome to MCU Explorer' }}
       </a>
     </div>
-    <div
-      class="flex flex-1 justify-center items-center animate-infinite-scroll-inverted"
-      :class="{ pause: downBannerHover }"
-    >
-      <a
-        v-for="movie in movies"
-        :key="movie.title + 3"
-        @click="selectOnTimelineEmiter(movie.id)"
-        class="w-[172.77px] h-[256px] m-10 animate-fade-in"
-      >
-        <img
-          @mouseenter="downBannerHover = true"
-          @mouseleave="downBannerHover = false"
-          :src="movie.cover_url"
-          :alt="movie.title + ' cover'"
-          class="h-full md:w-full rounded-sm animate-unblur hover:animate-blur cursor-pointer"
-        />
-      </a>
-      <a
-        v-for="movie in movies"
-        :key="movie.title + 4"
-        @click="selectOnTimelineEmiter(movie.id)"
-        class="w-[172.77px] h-[256px] m-10 animate-fade-in"
-      >
-        <img
-          @mouseenter="downBannerHover = true"
-          @mouseleave="downBannerHover = false"
-          :src="movie.cover_url"
-          :alt="movie.title + ' cover'"
-          class="h-full md:w-full rounded-sm animate-unblur hover:animate-blur cursor-pointer"
-        />
-      </a>
-    </div>
-    <!-- <img v-for="movie in movies" :key="movie.title" :src="movie.cover_url" :alt="movie.title + ' cover'" class="h-20"> -->
+    <scrolling-banner
+      :images="movies"
+      :invert="true"
+      @imageClicked="(id) => selectOnTimelineEmiter(id)"
+    />
   </div>
 </template>
 
 <script>
 import marvelAPI from '../api/marvelAPI'
+import ScrollingBanner from './subcomponents/ScrollingBanner.vue'
 
 export default {
+  components: { ScrollingBanner },
   name: 'Welcome',
   async mounted() {},
   props: ['movies'],
--- components/subcomponents/ScrollingBanner.vue
+++ components/subcomponents/ScrollingBanner.vue
@@ -1,62 +1,54 @@
 <template>
   <div
-    class="flex flex-1 justify-center items-center animate-infinite-scroll"
-    :class="{ pause: bannerHover }"
+    class="flex flex-1 justify-center items-center"
+    :class="{
+      pause: bannerHovered,
+      'animate-infinite-scroll': !invert,
+      'animate-infinite-scroll-inverted': invert,
+    }"
   >
-    <a
-      v-for="image in images"
-      :key="image.title + 1"
-      @mouseenter="
-        () => {
-          bannerHovered = true
-          isHovered = image.id
-        }
-      "
-      @mouseleave="
-        () => {
-          bannerHovered = false
-          isHovered = null
-        }
-      "
-      @click="$emit('imageClicked',image.id)"
-      class="w-[172.77px] h-[256px] m-10 animate-fade-in"
-    >
-      <h1
-        class="absolute w-[172.77px] h-[256px] text flex justify-center items-center text-center z-10 text-white font-bold pointer-events-none px-2"
-        :class="{
-          'animate-fade-in-fast': isHovered == image.id,
-          hidden: isHovered != image.id,
-        }"
+    <template v-for="dupe in 2">
+      <a
+        v-for="image in images"
+        :key="image.title + dupe"
+        @mouseenter="
+          () => {
+            bannerHovered = true
+            isHovered = image.id
+          }
+        "
+        @mouseleave="
+          () => {
+            bannerHovered = false
+            isHovered = null
+          }
+        "
+        @click="$emit('imageClicked', image.id)"
+        class="w-[172.77px] h-[256px] m-10 animate-fade-in"
       >
-        {{ image.title }}
-      </h1>
-      <img
-        :src="image.cover_url"
-        :alt="image.title + ' cover'"
-        class="h-full md:w-full rounded-sm animate-unblur hover:animate-blur cursor-pointer"
-      />
-    </a>
-    <a
-      v-for="image in images"
-      :key="image.title + 2"
-      @click="$emit('imageClicked',image.id)"
-      class="w-[172.77px] h-[256px] m-10 animate-fade-in"
-    >
-      <img
-        @mouseenter="bannerHovered = true"
-        @mouseleave="bannerHovered = false"
-        :src="image.cover_url"
-        :alt="image.title + ' cover'"
-        class="h-full md:w-full rounded-sm animate-unblur hover:animate-blur cursor-pointer"
-      />
-    </a>
+        <h1
+          class="absolute w-[172.77px] h-[256px] text flex justify-center items-center text-center z-10 text-white font-bold pointer-events-none px-2"
+          :class="{
+            'animate-fade-in-fast': isHovered == image.id,
+            hidden: isHovered != image.id,
+          }"
+        >
+          {{ image.title }}
+        </h1>
+        <img
+          :src="image.cover_url"
+          :alt="image.title + ' cover'"
+          class="h-full md:w-full rounded-sm animate-unblur hover:animate-blur cursor-pointer"
+        />
+      </a>
+    </template>
   </div>
 </template>
 
 <script>
 export default {
   name: 'ScrollingBanner',
-  props: ['images'],
+  props: ['images', 'invert'],
   data() {
     return {
       bannerHovered: false,
